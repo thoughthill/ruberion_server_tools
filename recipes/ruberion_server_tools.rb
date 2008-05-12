7636cfa80c7af5c212e7c414983a02b5297a3b23
fred <fred@freds-macbook.(none)>
2008-05-13 00:20:35 +0700

    Clean up and add attachment_fu

--- recipes/ruberion_server_tools.rb
+++ recipes/ruberion_server_tools.rb
@@ -59,6 +59,7 @@ namespace :config do
   task :shared_config do
     run "mkdir -p #{shared_path}/config"
     run "mkdir -p #{shared_path}/sessions"
+    run "mkdir -p #{shared_path}/attachment_fu"
     run "mkdir -p #{shared_path}/index" if using_ferret?
     if using_ultrasphinx?
       run "mkdir -p #{sphinx_db_path}"
@@ -178,12 +179,6 @@ namespace :deploy do
     deploy.mongrel.restart
   end
 
-  desc "Quick Deploy. Only update the current version"
-  task :quick_deploy, :roles => :app do
-    invoke_command "cd #{current_path} && svn update"
-    deploy.mongrel.restart
-  end
-  
   desc "Symlink Config Files"
   task :create_symlinks do
     run "ln -nfs #{shared_path}/config/ultrasphinx #{current_path}/config/ultrasphinx" if using_ultrasphinx?
@@ -193,6 +188,7 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/sessions #{current_path}/tmp/sessions"
     run "ln -nfs #{shared_path}/pids #{current_path}/tmp/pids"
     run "ln -nfs #{shared_path}/log #{current_path}/tmp/log"
+    run "ln -nfs #{shared_path}/tmp/attachment_fu #{current_path}/tmp/attachment_fu"
     config_files_to_symlink.each do |file|
       run "ln -nfs #{shared_path}/config/#{file} #{current_path}/config/#{file}" 
     end
@@ -242,88 +238,3 @@ namespace :nginx do
 end
 
 
-# ==========================
-# Sphinx Index Tasks
-# ==========================
-
-
-namespace :ultrasphinx do
-  
-  desc "Check if sphinx daemon is running"
-  task :sphinx_status, :roles => :app do
-    begin
-      invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:daemon:status"
-    rescue
-      puts "Seems like it's not deployed yet, skip"
-    end
-  end
-
-  desc "Ultrasphinx Bootstrap"
-  task :bootstrap, :roles => :app do    
-    if using_ultrasphinx?
-      invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:bootstrap"
-    else
-      puts "Enable ultrasphinx on deploy.rb first."
-    end
-  end
-
-  desc "Update Index"
-  task :index, :roles => :app do
-    if using_ultrasphinx? && ultrasphinx_configured?
-      invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:index"
-    else
-      puts "Enable ultrasphinx on deploy.rb or run 'cap config:ultrasphinx_default_base'"
-    end
-  end
-
-  desc "Configure Sphinx"
-  task :configure, :roles => :app do
-    if using_ultrasphinx?
-      if ultrasphinx_configured?
-        invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:configure"
-      else
-        puts "The file config/ultrasphinx/default.base is missing. Create it first"
-        puts " you can run cap config:ultrasphinx_default_base"
-      end
-    end
-  end
-
-  desc "Stop Sphinx daemon"
-  task :stop, :roles => :app do
-    begin
-      result = run_and_return("cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:daemon:status")
-      if result.match("Daemon is running")
-        invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:daemon:stop"
-      else
-        puts "Sphinx daemon Not Running."
-      end
-    rescue
-      puts "Seems like it's not deployed yet, skip"
-    end
-  end
-
-  desc "Start Sphinx daemon"
-  task :start, :roles => :app do
-    begin
-      result = run_and_return("cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:daemon:status")
-      if result.match("Daemon is stopped")
-        invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:daemon:start"
-      else
-        puts "Sphinx daemon already Running."
-      end
-    rescue
-      puts "Seems like it's not deployed yet, skip"
-      puts "maybe you should deploy first or run cap config:ultrasphinx_default_base"
-    end
-  end
-
-  desc "Restart Sphinx daemon"
-  task :restart, :roles => :app do
-    begin
-      invoke_command "cd #{current_path} && RAILS_ENV=#{rails_env} rake ultrasphinx:daemon:restart"
-    rescue
-      puts "Seems like it's not deployed yet, skip"
-      puts "maybe you should deploy first or run cap config:ultrasphinx_default_base"
-    end
-  end
-end
