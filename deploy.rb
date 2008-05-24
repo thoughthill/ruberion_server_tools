1c7892821334558ca99fe1a57666f10a71a2f118
Brian Szucs <brian@ruberion.com>
2008-05-24 08:41:32 -0400

    added defaults to deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -1,4 +1,4 @@
-require "erb"
+set(require "erb"
 
 if (ENV["staging"].nil? and ENV["production"].nil?)
   puts "._______________________________________________________________."
@@ -23,7 +23,6 @@ if ENV["staging"]
   set(:domain_names, ["staging.app.com"])
 end
 
-
 # "cap deploy production=true" will deploy to Production Server
 if ENV["production"]
   set(:webserver1, "10.11.11.11")
@@ -34,22 +33,24 @@ if ENV["production"]
   set(:domain_names, ["www.app.com", "app.com"])
 end
 
-### Servers
+# Please set the name of the domain/application here
 set(:application, "app.com")
+
+# Please set where to deploy to
+# Default value is to go to /var/www/apps/<application>/
 set(:deploy_to, "/var/www/apps/#{application}")
 
 ### 
-role :web, webserver1
-role :app, webserver1
-role :db,  webserver1, :primary => true
+role(:web, webserver1)
+role(:app, webserver1)
+role(:db,  webserver1, :primary => true)
 
 ### GitHub
 set(:repository, "git@github.com:xxx/xxx.git")
 set(:scm, "git")
 set(:scm_passphrase, "" )
+set(:git_enable_submodules, true)
 set(:deploy_via, :remote_cache)
-set(:git_enable_submodules, 1)
-
 
 ### ASSETS ###
 # links inside public/ folder,
@@ -58,3 +59,58 @@ set(:public_assets, %w(uploads) )
 set(:tmp_assets, %w(sessions cache pids) )
 # links inside config/ folder,
 set(:config_assets, %w(database.yml) )
+
+### These are all defaulted in ruberion_server_tools/recipes/defaults.rb
+
+# Enable Extensions MOST LIKELY TO CHANGE
+#set(:enable_memcached?,false)
+#set(:enable_ultrasphinx?,false)
+#set(:enable_memcached?, false)
+#set(:enable_ferret?, false)
+#set(:enable_push_server?, false)
+#set(:enable_juggernaut?, false)
+
+### Servers
+# Sets the environment in rails
+# Defaults rails_env to production
+#set(:rails_env, "production")
+
+# Domain names to add to vhost files
+#set(:vhost_domain_names, ["localhost"])
+
+# SSH user to login in the server
+#set(:user, "root")
+
+# Use sudo if you need to run privileged tasks
+#set(:use_sudo, false)
+
+# MOD_RAILS
+#set(:rails_max_pool_size, 1)
+#set(:rails_pool_idle_time, 3600)
+  
+# MONGREL
+#set(:mongrel_pid, "#{deploy_to}/#{shared_dir}/pids/mongrel.pid")
+#set(:mongrel_conf, "#{deploy_to}/#{shared_dir}/config/mongrel_cluster.yml")
+
+# Default sphinx variables
+#set(:sphinx_use_delta, false)
+#set(:sphinx_db_path, "#{shared_path}/index/sphinx/")
+#set(:sphinx_mem_limit, "128M")
+#set(:sphinx_delta_time, 14400)
+#set(:sphinx_server_port, 3312)
+#set(:sphinx_min_word_len, 3)
+
+# Backup
+#set(:backup_path,  "#{shared_path}/backups" }
+#set(:skip_backup_tables, ['sessions'])
+
+## Default values
+# Database #
+#set(:development_db, "#{application.gsub('.', '_')}_development")
+#set(:production_db, "#{application.gsub('.', '_')}_production")
+#set(:test_db, "#{application.gsub('.', '_')}_test")
+
+# default socket for Gentoo Linux
+#set(:mysql_socket, "/var/run/mysqld/mysqld.sock")
+#set(:mysql_user, "root")
+#set(:mysql_password, "")
